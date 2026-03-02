adcf6e2c9625db8169724e4f9be0eeea11e8734e
invitacionesweb <invitacionesweb.info@gmail.com>
2026-03-02 08:33:06 -0300

    a
    a

---
+++ form.js
@@ -0,0 +1,25 @@
+
+
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbwu1rVxldP7enCbr1PlSYp5VLHD195dUJAkZS-avJt3FVIap_PBKYpBFmB8c64wWEGJ/exec';
+const form = document.forms['asistenciaform'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  Swal.fire({
+    title: 'Enviando...',
+    text: 'Por favor, esperá un momento',
+    allowOutsideClick: false,
+    didOpen: () => Swal.showLoading()
+  });
+
+  fetch(scriptUrl, { method: 'POST', body: new FormData(form) })
+    .then(res => {
+      if (!res.ok) throw new Error('Error de red');
+      Swal.fire('¡MUCHAS GRACIAS!', 'Formulario enviado', 'success');
+    })
+    .then(() => setTimeout(() => location.reload(), 1500))
+    .catch(() =>
+      Swal.fire('Error', 'No se pudo enviar el formulario', 'error')
+    );
+});
